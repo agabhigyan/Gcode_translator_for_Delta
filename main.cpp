5e51c929f39780dbb9c8cfb6ed34980f62af5bd8
agabhigyan <49595252+agabhigyan@users.noreply.github.com>
2019-04-16 00:45:08 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
+/*Fore translating the G-Codes the file format needs to be changed from ".g" or ".gcode" or ".gco" to ".txt". In the same directory where the main.cpp file is stored, there must be at least two more files - "settings.txt" and "targets.txt". Save the G-Code files in the same directory
+Calibration:
+Upload cartesian firmware Sprinter/Marlin to your CNC machine. Connect your 
+*/
 #include<iostream>
 #include<math.h>
 #include<fstream>
 #include<stdlib.h>
 #include<string>
 #include <sstream>
-#include <ctype.h>
-#include <stdio.h>
-#include<sstream>
 float cal_x(float x,float y, float z, float d, float r, float R, float l,float H)
 { 	float a1x=x+r;
 	float a1y=y;
@@ -43,9 +44,18 @@ return Cz;
 using namespace std;
 int main()
 {       float x[10]; char ch;int gcde=0;
-        
+        bool got_values=false;
+        bool got_F=false;
+	bool got_E=false;
+        bool got_only_z=false;
+        bool check_quality=false;
+	bool got_G_M=false;
+	char ch_x;char ch_y;char ch_z; char ch_f;char g28;
+        float x_value=0.0;float y_value=0.0;float z_value=0.0;float e_value=0.0;  float f_value=0.0; 
+        float x_value_new=0.0;float y_value_new=0.0;float z_value_new=0.0; 
+	
     	char buffer[80];  
-        char data[80];char files[80];string str1="square.txt";
+        char data[80];char files[80];
         string str[10];//name of the files will be stored
         string strNew[10];
         string mod="modified_for_Delta_";
@@ -90,17 +100,16 @@ int main()
 
 for(int k=1;k<=NUMofFiles;k++) /////////starting of for loop
        {  
-        bool got_values=false;
-        bool got_F=false;
-        bool got_only_z=false;
-        bool check_quality=false;
-	char ch_x;char ch_y;char ch_z; char ch_f;char g28;
-        float x_value=0.0;float y_value=0.0;float z_value=0.0; float f_value=0.0; 
-        float x_mid=0.0;float y_mid=0.0;
-        float x_value_new=0.0;float y_value_new=0.0;float z_value_new=0.0; 
-	char ch;int gcde=0;
-       
-	ifstream infile3(str[k].c_str()); //reads the file str1 for G-Code
+       got_values=false;
+       got_F=false;
+       got_E=false;
+       got_only_z=false;
+       check_quality=false;
+	got_G_M=false;
+	x_value=0.0;y_value=0.0;z_value=0.0;e_value=0.0;f_value=0.0; 
+        x_value_new=0.0;y_value_new=0.0;z_value_new=0.0; 
+	gcde=0;
+       	ifstream infile3(str[k].c_str()); //reads the file str1 for G-Code
         if (! infile3)
 	{ cout<<"\n The target files do not exist"<<endl;
 	exit(1);
@@ -116,20 +125,45 @@ for(int k=1;k<=NUMofFiles;k++) /////////starting of for loop
           {infile3>>gcde;got_only_z=false;got_values=false;got_F=false;
             if(gcde==0)
               {
-                infile3>>ch_x;
-                if(ch_x=='X')  {infile3>>x_value;//reads x value
-                               infile3>>ch_y;
-                 		if(ch_y=='Y')  {infile3>>y_value;
-                 		 infile3>>ch_z;
-                		if(ch_z=='Z')  {infile3>>z_value;got_values=true;
-				infile3>>ch_z; if(ch_z=='F')  {infile3>>f_value; got_F=true;}
-                                                        }} } 
-		 if(ch_x=='Y')  {infile3>>y_value;
-                 		 infile3>>ch_z;
-                		 if(ch_y=='Z')  {infile3>>y_value;} }
-                 if(ch_x=='Z')  {infile3>>z_value;got_only_z=true;}
-                
-               } //done reading x,y and z values...........G0 code
+                infile3>>ch;
+      switch(ch)
+{ 
+case 'X':  infile3>>x_value;//reads x value
+           infile3>>ch;
+           switch(ch)      		
+           {   
+           case 'Y': infile3>>y_value; got_values=true;
+ 		        infile3>>ch;
+		   switch(ch)
+                   {                	
+                   case 'Z':infile3>>z_value;
+                            break;
+                   case 'E':infile3>>e_value;
+                            got_E=true;
+                            break;
+	   	   case 'F':infile3>>f_value; got_F=true;
+                            break;
+                   case 'G':got_G_M=true;
+                            break;
+                    }
+                        break;
+           case 'Z':cout<<"lala:";
+		    break;
+	   case 'E':cout<<"lala:";
+		    break;
+           case 'G':cout<<"lala:";
+		    break;
+           }
+	   break;                                                
+case 'Y':  infile3>>y_value;
+           infile3>>ch;
+           if(ch=='Z')  {infile3>>y_value;} 
+         break;
+case 'Z':  infile3>>z_value;got_only_z=true;
+         break;    
+case 'E':  infile3>>e_value;
+         break;       
+}     } //done reading x,y and z values...........G0 code
                   
                 
               if(gcde==28) 
@@ -146,11 +180,11 @@ for(int k=1;k<=NUMofFiles;k++) /////////starting of for loop
                y_value_new=cal_y(x_value,y_value,z_value,x[4],x[2],x[3],x[0],x[1]);
 	       z_value_new=cal_z(x_value,y_value,z_value,x[4],x[2],x[3],x[0],x[1]);
                 outfile<<"G0"<<" "<<"X"<<x_value_new<<" "<<"Y"<<y_value_new<<" Z"<<z_value_new;
-                if(got_F==true) outfile<<" "<<"F"<<f_value;
+		if(got_E==true) outfile<<" "<<"E"<<e_value;                
+		if(got_F==true) outfile<<" "<<"F"<<f_value;
                  outfile<<" ;original point\n";
                }
-             //if(got_only_z==true)outfile<<"G0"<<" "<<"Z"<<z_value<<endl;
-          }//end of reading G-value
+            }//end of reading G-value
          }//end of while
 } //end of for loop
 
